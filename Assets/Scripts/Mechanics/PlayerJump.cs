67704490f842fd5ed56f9dd60c21666f9e5d2f50
Syny <s.raouf@student.isartdigital.com>
2018-06-27 08:41:22 -0400

    Prefabs:player, gamemanager, inputmanager

--- Assets/Scripts/Mechanics/PlayerJump.cs
+++ Assets/Scripts/Mechanics/PlayerJump.cs
@@ -8,9 +8,6 @@ public class PlayerJump : MonoBehaviour {
 
     [Range(1, 20)]
     [SerializeField] private float jumpVelocity;
-    [SerializeField] private float fallMultiplier = 2.5f;
-    [SerializeField] private float lowJumpMultiplier = 2f;
-
     private Rigidbody2D rb;
 
 	// Use this for initialization
@@ -22,18 +19,7 @@ public class PlayerJump : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        Debug.Log(rb.velocity.y);
-
         if (InputManager.Jump() && Mathf.Abs(rb.velocity.y) < 0.01f)
             rb.velocity = new Vector2(0, jumpVelocity);
-            //rb.velocity = Vector2.up * jumpVelocity;
 	}
-
-    private void FixedUpdate()
-    {
-        //if (rb.velocity.y < 0)
-        //    rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-        //else if(rb.velocity.y > 0 && !InputManager.Jump())
-        //    rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
-    }
 }
